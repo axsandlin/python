1cd6914a1ecacd33c2a45eaab8799c43202b552c
Aubri Sandlin <axsandlin@aubri-laptop.h8n.com>
2014-02-20 07:22:01 -0800

    james fixing json generation

--- mp3_info.py
+++ mp3_info.py
@@ -20,31 +20,10 @@ music_dir = "/Volumes/music/unsorted/Passion_Pit/Passion_Pit_-_Gossamer_mp3/"
 folders = {}
 def getEyeD3Info(d):
    songs = []
-#open file
-#   f = open('/Users/axsandlin/workspaces/python/mp3_list','w')
-   
-#returns a list of the directory
    music_list = os.listdir(d)
    
    type = ""
    json_string = ""
-   
-#   for x in music_list:
-      
-#checks to see if directory object is a file       
-#      if os.path.isfile(d + '/' + x):
-#         type = "file" 
-#      else:
-#         type = "dir"
-#      
-#      json_string = json.dumps({'type': type, 'name': x}, sort_keys=True,
- #                                   indent=4, separators=(',', ': '))
-     
-      #print json_string
-#      f.write(json_string)
-      
-#close file
-#  f.close()
    json_txt = ""
    for x in music_list:  
       if os.path.isfile(d + '/' + x):
@@ -53,10 +32,6 @@ def getEyeD3Info(d):
             print d + '/' + x + " is mp3"
             tag = eyeD3.Tag()
             tag.link(d + '/' + x) 
-            #print tag
-            #print tag.getTitle()
-            #print tag.getArtist()
-            #print tag.getAlbum()
             song = {
                'file_name': x, 
                'artist' : tag.getArtist(), 
@@ -66,49 +41,14 @@ def getEyeD3Info(d):
                'track': tag.getTrackNum()
             }
             songs.append(song)
-            
-            #json_txt = json.load(
-            #                  {
-            #                     'folder': d
-            #                     , 
-            #                     'songs':  
-            #                        [
-            #                           {
-            #                              'name': x,
-            #                              #'file_name': tag.getTitle(), 
-            #                              #'artist' : tag.getArtist(), 
-            #                              #'album': tag.getAlbum(), 
-            #                              #'title': tag.getTitle(), 
-            #                              #'genre': str(tag.getGenre()), 
-            #                              #'track': tag.getTrackNum()
-            #                           },
-            #                        ]
-            #                  }
-            #                )
-         #else:
-         #   print "Not an MP3 -> " + d + '/' + x 
-   #print json.encode(songs)
-   
    dir_data = {'folder': d, 'songs': songs}
-   print json.dumps(dir_data, sort_keys=True, indent=2, separators=(',', ': '))
-   #json.dumps(json_txt, sort_keys=True,
-   #   indent=3, separators=(',', ' : '))
-#print json.dumps({'4': 5, '6': 7}, sort_keys=True,
-#                    indent=4, separators=(',', ': '))  
-
-#{
-#    "4": 5,
-#    "6": 7
-#}
-
-#json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',',':'))
-#'[1,2,3,{"4":5,"6":7}]'   
-   
-
-   
+   return dir_data
    
 #call my method and pass it a directory.
-getEyeD3Info(music_dir)
+dir_eyed3_data = getEyeD3Info(music_dir)
+
+print json.dumps(dir_eyed3_data, sort_keys=True, indent=2, separators=(',', ': '))
+   
 
 
 #{  {
